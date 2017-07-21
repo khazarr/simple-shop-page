b7a22666fa14c79bfdb4fb2058848b9b5c097f8a
Karol Swieca <swieca.karol@gmail.com>
2017-07-21 21:09:47 +0200

    working thumbnail

--- js/index.js
+++ js/index.js
@@ -1,4 +1,54 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
+    
+    //product view
+    
+    
+    //add to cart
+    
+    
+    //thumbnail
+    //change pic on enter and add border, remove border on other pics
+    
+    //pic 1
+    $("#pic1").mouseenter(function(){
+        $("#thumb").children().css({border: "2px solid #d4d4d4"});
+        $("#pic1").css({border: "4px solid black"});
+        $("#big-pic").attr("src","pics/pic1.jpg");
+    });
+    
+    //pic2
+    $("#pic2").mouseenter(function(){
+        $("#thumb").children().css({border: "2px solid #d4d4d4"});
+        $("#pic2").css({border: "4px solid black"});
+        $("#big-pic").attr("src","pics/pic2.jpg");
+    });
+    
+    //pic3
+    $("#pic3").mouseenter(function(){
+        $("#thumb").children().css({border: "2px solid #d4d4d4"});
+        $("#pic3").css({border: "4px solid black"});
+        $("#big-pic").attr("src","pics/pic3.jpg");
+    });
+    
+     //pic4
+    $("#pic4").mouseenter(function(){
+        $("#thumb").children().css({border: "2px solid #d4d4d4"});
+        $("#pic4").css({border: "4px solid black"});
+        $("#big-pic").attr("src","pics/pic4.jpg");
+    });
+    
+     //pic5
+    $("#pic5").mouseenter(function(){
+        $("#thumb").children().css({border: "2px solid #d4d4d4"});
+        $("#pic5").css({border: "4px solid black"});
+        $("#big-pic").attr("src","pics/pic5.jpg");
+    });
+  
+    
+    
+    
+    
+    
 });
