96fd96447c335c9842045e280c7a2e09679df8db
Karol Swieca <swieca.karol@gmail.com>
2017-07-22 09:46:47 +0200

    all functionalities now in index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,18 @@ $( document ).ready(function() {
    
 
     //product view -send
+          (function (srcjs) {
+               window._edrone = window._edrone || {};
+
+               _edrone.app_id = '59722eee745af';
+
+               var doc = document.createElement('script');
+               doc.type = 'text/javascript';
+               doc.async = true;
+               doc.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + srcjs;
+               var s = document.getElementsByTagName('script')[0];
+               s.parentNode.insertBefore(doc, s);
+           })("//d3bo67muzbfgtl.cloudfront.net/edrone_2_0.js");
        _edrone.app_id = '59722eee745af';
        _edrone.version = '1.0.0';
        _edrone.action_type = 'product_view';
@@ -20,9 +32,35 @@ $( document ).ready(function() {
        _edrone.product_category_ids = '2~3~4';
        _edrone.product_category_names = 'Główna~Buty~Sportowe~Nike';
     
-    //add to cart
+    //add to cart - send trace
     $("#basket-add").click(function(){
-        $.getScript('js/add-to-cart.js');
+        //$.getScript('js/add-to-cart.js');
+        (function (srcjs) {
+            window._edrone = window._edrone || {};
+
+            _edrone.app_id = '59722eee745af';
+
+            var doc = document.createElement('script');
+            doc.type = 'text/javascript';
+            doc.async = true;
+            doc.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + srcjs;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(doc, s);
+        })("//d3bo67muzbfgtl.cloudfront.net/edrone_2_0.js");       
+
+
+        _edrone.app_id = '59722eee745af';
+       _edrone.version = '1.0.0';
+       _edrone.action_type = 'add_to_cart';
+       _edrone.platform_version = '1.1.26';
+       _edrone.platform = 'customer';
+       _edrone.product_skus = '525104021';
+       _edrone.product_ids = '021';     
+       _edrone.product_titles = 'BUTY NIKE SUPER';
+       _edrone.product_images = 'http://i.imgur.com/quGZsz2.jpg';
+       _edrone.product_urls = 'index.html';
+       _edrone.product_category_ids = '2~3~4';
+       _edrone.product_category_names = 'Główna~Buty~Sportowe~Nike';
         alert("Dodano do koszyka");
         
  
