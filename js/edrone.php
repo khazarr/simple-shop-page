37fabc248bc3bd377f7aa23286a706f3cd0f800d
Karol Swieca <swieca.karol@gmail.com>
2017-08-12 20:37:11 +0200

    works as expected

---
+++ js/edrone.php
@@ -0,0 +1,45 @@
+<?php
+
+  $content = $_POST['data'];
+  //$decoded = json_decode($content);
+
+  // foreach ($decoded as $key => $value) {
+  //   echo "{$key} => {$value} \n";
+  // }
+
+
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.edrone.me/trace.php",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    //CURLOPT_POSTFIELDS => "app_id=598ee28767cf4&version=1.0.0&action_type=product_view&platform_version=1.1.26&platform=prestashop&sender_type=server&product_ids=123&product_titles=BUTY%20NIKE%20STEFAN&product_images=http%3A%2F%2Fi.imgur.com%2FquGZsz2.jpg&product_urls=http%3A%2F%2Fi.imgur.com%2FquGZsz2.jpg&product_category_ids=2~3~4&product_category_names=G%C5%82%C3%B3wna~Buty~Sportowe~Nike",
+    CURLOPT_POSTFIELDS => $content,
+    CURLOPT_HTTPHEADER => array(
+      "cache-control: no-cache",
+      "content-type: application/x-www-form-urlencoded",
+      "postman-token: 535a0f1b-27d7-4fdb-3771-9f8134fc285a"
+    ),
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+
+
+  echo "DOSZŁO";
+
+ ?>
--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+//function
+function jsonToUri(json) {
+  return encodeURIComponent(JSON.stringify(json));
+}
+
+function xwwwfurlenc(srcjson){
+    if(typeof srcjson !== "object")
+      if(typeof console !== "undefined"){
+        console.log("\"srcjson\" is not a JSON object");
+        return null;
+      }
+    u = encodeURIComponent;
+    var urljson = "";
+    var keys = Object.keys(srcjson);
+    for(var i=0; i <keys.length; i++){
+        urljson += u(keys[i]) + "=" + u(srcjson[keys[i]]);
+        if(i < (keys.length-1))urljson+="&";
+    }
+    return urljson;
+}
+
 
 // edrone init
 (function (srcjs) {
@@ -26,11 +47,6 @@
      s.parentNode.insertBefore(doc, s);
  })("//api.edrone.me/edrone_2_0.js");
 
-    //add to cart - send trace
-    $("#basket-add").click(function(){
-       _edrone.action_type = 'add_to_cart';
-       _edrone.init();
-    })
 
 
 
@@ -42,7 +58,7 @@ var products = [
     product_url:"http://i.imgur.com/quGZsz2.jpg",
     product_category_id:"2~3~4",
     product_category_name:"Główna~Buty~Sportowe~Nike",
-    price: "259",
+    price: 259,
     active: true
   },
   {
@@ -51,7 +67,7 @@ var products = [
     product_url:"https://static.eastend.pl/e2pl/zdjecia/buty-nike-zoom-stefan-janoski-cnvs-642-00-640c.jpg",
     product_category_id:"2~3~4",
     product_category_name:"Główna~Buty~Sportowe~Nike",
-    price: "259",
+    price: 209,
     active: false
   },
   {
@@ -60,7 +76,7 @@ var products = [
     product_url:"https://static.eastend.pl/e2pl/zdjecia/buty-nike-zoom-stefan-janoski-603-00-640c.jpg",
     product_category_id:"2~3~4",
     product_category_name:"Główna~Buty~Sportowe~Nike",
-    price: "259",
+    price: 319,
     active: false
   },
   {
@@ -69,7 +85,7 @@ var products = [
     product_url:"https://static.eastend.pl/e2pl/zdjecia/buty-nike-zoom-stefan-janoski-215-00-640c.jpg",
     product_category_id:"2~3~4",
     product_category_name:"Główna~Buty~Sportowe~Nike",
-    price: "259",
+    price: 309,
     active: false
   },
   {
@@ -78,21 +94,106 @@ var products = [
     product_url:"https://static.eastend.pl/e2pl/zdjecia/buty-nike-zoom-stefan-janoski-409-00-640c.jpg",
     product_category_id:"2~3~4",
     product_category_name:"Główna~Buty~Sportowe~Nike",
-    price: "259",
+    price: 259,
     active: false
   },
 
 ];
 
+//basket array
+
+var basket = [];
 
 
-// thubnails change picture
+//calculate basket pirice
+
+function getBasketPrice(bsk){
+  var total = 0;
+
+  for (var i = 0; i < bsk.length ; i++){
+    total += parseInt(bsk[i]['price']);
+  }
+
+  return total;
+}
+
+// clear basket
+
+function basketClear() {
+  localStorage.removeItem("edrone_basket");
+  $("#buy-button").hide();
+  $("#basket-info").hide();
+  $("#clear-basket").hide();
+  basket = [];
+
+}
+
+    //hide buy-button until basket not empty - check local storage
+    if(localStorage.getItem("edrone_basket") === null ){
+      $("#buy-button").hide();
+      $("#basket-info").hide();
+      $("#clear-basket").hide();
+
+    }
+    else{
+      // load basket from local storage
+      basket = JSON.parse(localStorage.getItem("edrone_basket"));
+      console.log("BASKET HERE");
+      console.log(basket);
+
+      $("#basket-info").html("Elementy w koszyku: " + basket.length);
+    }
+
+
+
+    //add to cart - send trace
+    $("#basket-add").click(function(){
+
+      //basket not empty, show buy-button
+      $("#buy-button").show();
+      $("#basket-info").show();
+      $("#clear-basket").show();
+
+
+
+      for (var i = 0; i < products.length ; i++){
+        if(products[i]['active']){
+
+          //append basket array
+          basket.push(products[i]);
+
+          //store basket in localstorage
+          localStorage.setItem("edrone_basket",JSON.stringify(basket));
+
+          //send edrone trace
+          _edrone.product_ids = products[i]['product_id'];
+          _edrone.product_titles = products[i]['product_title'];
+          _edrone.product_images = products[i]['product_url'];
+          _edrone.product_urls = 'https://www.wp.pl/';
+          _edrone.product_category_ids = '2~3~4';
+          _edrone.product_category_names = 'Główna~Buty~Sportowe~Nike';
+          _edrone.action_type = 'add_to_cart';
+          _edrone.init();
+          console.log(basket);
+
+
+          //update basket info
+          $("#basket-info").html("Elementy w koszyku: " + basket.length);
+        }
+      }
+      // _edrone.action_type = 'add_to_cart';
+      //_edrone.init();
+    })
+
+
+
+// thubnails change picture / add border
     $(".small-pic").mouseenter(function(){
       var that = $(this);
         $("#thumb").children().css({border: "2px solid #d4d4d4"});
         that.css({border: "4px solid black"});
         //$("#big-pic").attr("src","pics/pic" + that.data('id') + ".jpg");
-        $("#big-pic").attr("src",products[that.data('id')]['product_url']);
+        //$("#big-pic").attr("src",products[that.data('id')]['product_url']);
         //console.log(thad)
     });
 
@@ -102,6 +203,11 @@ var products = [
       console.log("Product with id:" + that.data('id')+ "clicked");
 
       //change page view
+      $("#product-name").html(products[that.data('id')]['product_title']);
+      $("#product-code").html(products[that.data('id')]['product_id']);
+      $("#price").html(products[that.data('id')]['price'] + ",00 zł");
+      $("#big-pic").attr("src",products[that.data('id')]['product_url']);
+
 
       //change state of product to active
       for(var i = 0; i< products.length ; i++){
@@ -127,7 +233,127 @@ var products = [
     });
 
 
+    //clearing basket
+    $("#clear-basket").click(function() {
+      localStorage.removeItem("edrone_basket");
+      $("#buy-button").hide();
+      $("#basket-info").hide();
+      $("#clear-basket").hide();
+      basket = [];
 
+    });
+
+
+
+    //buy-button logic
+    $("#buy-button").click(function () {
+      $("#basket-quantity-total").html("Elementy w koszyku: " + basket.length);
+      $("#basket-value-total").html("Wartość koszyka: " + getBasketPrice(basket) +" zł");
+    });
+
+    //place order
+    $("#place-order").click(function () {
+        console.log("Kupuje!");
+        var order = {};
+
+        //prepare data for POST
+        // ids, titles, imgages, urls,categories,prod
+        var ids = "";
+        for(var i = 0; i < basket.length ; i++){
+          ids += basket[i]['product_id'] + "|";
+        }
+        ids = ids.slice(0,-1);
+
+        order['ids'] = ids;
+
+        var titles = "";
+        for(var i = 0; i < basket.length ; i++){
+          titles += basket[i]['product_title'] + "|";
+        }
+        titles = titles.slice(0,-1);
+
+        order['titles'] = titles;
+
+        var imgs = "";
+        for(var i = 0; i < basket.length ; i++){
+          imgs += basket[i]['product_url'] + "|";
+        }
+        imgs = imgs.slice(0,-1);
+
+        order['imgs'] = imgs;
+        order['urls'] = imgs;
+
+
+        var catIds = "";
+        var counts = "";
+        var catNames = "";
+        for(var i = 0; i < basket.length ; i++){
+          catIds += basket[i]['product_category_id'] + "|";
+          counts += "1|";
+          catNames += basket[i]['product_category_name'] + "|";
+        }
+        catIds = catIds.slice(0,-1);
+        counts = counts.slice(0,-1);
+        catNames = catNames.slice(0,-1);
+        order['catIds'] = catIds;
+        order['catNames'] = catNames;
+        order['counts'] = counts;
+
+
+
+        order['email'] = $("#email-form").val();
+        order['first_name'] = $("#name-form").val();
+        order['last_name'] = $("#surname-form").val();
+        order['subscribe'] = $("#subscribe-form").is(':checked') ? 1 : 0;
+        order['city'] = $("#city-form").val();
+        order['payment_value'] = getBasketPrice(basket);
+
+
+       _edrone.action_type = 'order';
+       _edrone.email = order['email'];
+       _edrone.first_name = order['first_name'];
+       _edrone.last_name = order['last_name'];
+       _edrone.subscriber_status = order['subscribe'];
+       _edrone.product_ids = order['ids'];
+       _edrone.product_titles = order['titles'];
+       _edrone.product_images = order['imgs'];
+       _edrone.product_urls = order['urls'];
+       _edrone.product_category_ids = order['catIds'];
+       _edrone.product_category_names = order['catNames'];
+       _edrone.product_counts = order['counts'];
+       _edrone.order_id = '4321';
+       _edrone.country = 'Polska';
+       _edrone.city = order['city'];
+       _edrone.base_currency = 'PLN';
+       _edrone.order_currency = 'PLN';
+       _edrone.base_payment_value = order['payment_value'];
+       _edrone.order_payment_value = order['payment_value'];
+
+        //AJAX send
+        var json_upload = JSON.stringify(_edrone);
+        var edroneURI = xwwwfurlenc(_edrone);
+        //console.log(json_upload);
+        //console.log(edroneURI);
+
+        $.ajax({
+            url: "js/edrone.php",
+            type: "post",
+            data: {data:edroneURI} ,
+            success: function (response) {
+               // you will get response from your php page (what you echo or print)
+               console.log(response);
+               alert("Zamówienie przetworzone pomyślnie");
+               $("#myModal").modal('hide');
+               basketClear();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+               console.log(textStatus, errorThrown);
+            }
+
+
+        });
+
+    })
     //size button style change
 
     $("#size-btn-1").click(function(){
